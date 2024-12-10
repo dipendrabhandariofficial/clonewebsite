9293a1269a6e06e6b6cae16c55205acb546588b2
Dipendra Bhandari <dipendrabhandari0000@gmail.com>
2024-12-10 15:08:36 +0545

    work updated with images

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,24 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
 const About = () => {
+  const skills = [
+    "HTML5",
+    "CSS3",
+    "Sass",
+    "Javascript",
+    "jQuery",
+    "React",
+    "Firebase",
+    "RESTful APIs",
+    "Wordpress",
+    "Squarespace",
+    "Adobe Creative Cloud",
+    "Sketch",
+    "SEO practices",
+    "Git collaboration",
+    "Responsive web design",
+    "Web accessibility",
+  ];
   useGSAP(() => {
     gsap.from(".image2", {
       opacity: 0,
@@ -73,24 +91,7 @@ const About = () => {
           <p>My skills</p>
         </div>
         <div className="skillslists grid grid-rows-4 grid-cols-4 gap-4 h-auto w-auto animate-fadeInUp delay-500 duration-75 ">
-          {[
-            "HTML5",
-            "CSS3",
-            "Sass",
-            "Javascript",
-            "jQuery",
-            "React",
-            "Firebase",
-            "RESTful APIs",
-            "Wordpress",
-            "Squarespace",
-            "Adobe Creative Cloud",
-            "Sketch",
-            "SEO practices",
-            "Git collaboration",
-            "Responsive web design",
-            "Web accessibility",
-          ].map((skill, index) => (
+          {skills.map((skill, index) => (
             <div
               key={index}
               className="skill-item text-left font-sans text-[18px] font-[100] text-[#2c2c2c] "
@@ -133,78 +134,90 @@ const About = () => {
 
       {/* blog  */}
       <div className=" w-full h-auto    ">
-      <div className="h-[10%] text-center text-[#2c2c2c] font-[serif] font-[100] text-[30px]  tracking-[2px] pt-8">
-        <h3>Sometimes i write things</h3>
+        <div className="h-[10%] text-center text-[#2c2c2c] font-[serif] font-[100] text-[30px]  tracking-[2px] pt-8">
+          <h3>Sometimes i write things</h3>
+        </div>
+        <div className="h-[80] w-full grid grid-cols-3 gap-5 pt-16 px-[138px]">
+          <div className="hover:shadow-xl delay-100 duration-[400ms]  shadow-md flex flex-col justify-center items-center px-4 py-4">
+            <div className="h-[60%]">
+              <img
+                className="image2"
+                src="https://www.kaystocks.com/assets/blog-graphics-banner-04.png"
+                alt=""
+              />
+            </div>
+            <div className="h-[40%] flex flex-col pt-8 pr-10">
+              <h4 className="text-[21px] capitalize  font-[lora] text-[#2c2c2c] tracking-[1px] font-[100]">
+                {" "}
+                User-testing my Pins N’ Patches app
+              </h4>
+              <p className="text-[#2c2c2c]  tracking-[1px] font-[open serif]  font-[300]">
+                {" "}
+                April 28, 2020
+              </p>
+            </div>
+          </div>
+
+          <div className="hover:shadow-xl delay-100 duration-[400ms]  shadow-md flex flex-col justify-center items-center px-4 py-4">
+            <div className="h-[60%]">
+              <img
+                className="image2"
+                src="https://www.kaystocks.com/assets/blog-graphics-banner-03-03.png"
+                alt=""
+              />
+            </div>
+            <div className="h-[40%] flex flex-col pt-8 pr-10">
+              <h4 className="text-[21px] capitalize  font-[lora] text-[#2c2c2c] tracking-[1px] ">
+                {" "}
+                My Juno Journey | Pt. 2
+              </h4>
+              <p className="text-[#2c2c2c]  tracking-[1px]  font-[open serif]  font-[300]">
+                {" "}
+                April 28, 2020
+              </p>
+            </div>
+          </div>
+
+          <div className=" hover:shadow-xl shadow-md flex flex-col justify-center items-center px-4 py-4">
+            <div className="h-[60%]">
+              <img
+                className="image2"
+                src="https://www.kaystocks.com/assets/blog-graphics-banner-02-02.png"
+                alt=""
+              />
+            </div>
+            <div className="h-[40%] flex flex-col pt-8 pr-10">
+              <h4 className="text-[21px] capitalize  font-[lora] text-[#2c2c2c] tracking-[1px]">
+                {" "}
+                My Juno Journey | Pt. 1
+              </h4>
+              <p className="text-[#2c2c2c]  tracking-[1px]  font-[open serif]  font-[300]">
+                {" "}
+                April 28, 2020
+              </p>
+            </div>
+          </div>
+
+          <div className="hover:shadow-xl shadow-md flex flex-col justify-center items-center px-4 py-4">
+            <div className="h-[60%]">
+              <img
+                className="image2"
+                src="https://www.kaystocks.com/assets/blog-graphics-banner-01.png"
+                alt=""
+              />
+            </div>
+            <div className="h-[40%] flex flex-col pt-8 pr-10">
+              <h4 className="text-[21px] capitalize font-[lora] text-[#2c2c2c] tracking-[1px] ">
+                Why I took a break from social media
+              </h4>
+              <p className="text-[#2c2c2c]  tracking-[1px] font-[open serif]  font-[300] ">
+                {" "}
+                April 28, 2020
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="h-[80] w-full grid grid-cols-3 gap-5 pt-16 px-[138px]">
-        <div className="hover:shadow-xl delay-100 duration-[400ms]  shadow-md flex flex-col justify-center items-center px-4 py-4">
-          <div className="h-[60%]">
-            <img
-              className="image2"
-              src="https://www.kaystocks.com/assets/blog-graphics-banner-04.png"
-              alt=""
-            />
-          </div>
-          <div className="h-[40%] flex flex-col pt-8 pr-10">
-            <h4 className="text-[21px] capitalize  font-[lora] text-[#2c2c2c] tracking-[1px] font-[100]">
-              {" "}
-              User-testing my Pins N’ Patches app
-            </h4>
-            <p className="text-[#2c2c2c]  tracking-[1px] font-[open serif]  font-[300]"> April 28, 2020</p>
-          </div>
-        </div>
-
-        <div className="hover:shadow-xl delay-100 duration-[400ms]  shadow-md flex flex-col justify-center items-center px-4 py-4">
-          <div className="h-[60%]">
-            <img
-              className="image2"
-              src="https://www.kaystocks.com/assets/blog-graphics-banner-03-03.png"
-              alt=""
-            />
-          </div>
-          <div className="h-[40%] flex flex-col pt-8 pr-10">
-            <h4 className="text-[21px] capitalize  font-[lora] text-[#2c2c2c] tracking-[1px] ">
-              {" "}
-              My Juno Journey | Pt. 2
-            </h4>
-            <p className="text-[#2c2c2c]  tracking-[1px]  font-[open serif]  font-[300]"> April 28, 2020</p>
-          </div>
-        </div>
-
-        <div className=" hover:shadow-xl shadow-md flex flex-col justify-center items-center px-4 py-4">
-          <div className="h-[60%]">
-            <img
-              className="image2"
-              src="https://www.kaystocks.com/assets/blog-graphics-banner-02-02.png"
-              alt=""
-            />
-          </div>
-          <div className="h-[40%] flex flex-col pt-8 pr-10">
-            <h4 className="text-[21px] capitalize  font-[lora] text-[#2c2c2c] tracking-[1px]">
-              {" "}
-              My Juno Journey | Pt. 1
-            </h4>
-            <p className="text-[#2c2c2c]  tracking-[1px]  font-[open serif]  font-[300]"> April 28, 2020</p>
-          </div>
-        </div>
-
-        <div className="hover:shadow-xl shadow-md flex flex-col justify-center items-center px-4 py-4">
-          <div className="h-[60%]">
-            <img
-              className="image2"
-              src="https://www.kaystocks.com/assets/blog-graphics-banner-01.png"
-              alt=""
-            />
-          </div>
-          <div className="h-[40%] flex flex-col pt-8 pr-10">
-            <h4 className="text-[21px] capitalize font-[lora] text-[#2c2c2c] tracking-[1px] ">
-              Why I took a break from social media
-            </h4>
-            <p className="text-[#2c2c2c]  tracking-[1px] font-[open serif]  font-[300] "> April 28, 2020</p>
-          </div>
-        </div>
-      </div>
-    </div>
     </>
   );
 };
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
 
 const Work = () => {
   return (
-    <div className='bg-black h-screen w-screen'>
-      
+    <div className=" min-h-screen  w-screen flex flex-col gap-12  items-start px-32 pt-8 pb-4 tracking-[1px] text-[#2c2c2c] ">
+      <div className="text-[64px] font-serif  lowercase leading-none">Work</div>
+      <div className=" flex items-start gap-4 text-[14px]">
+        <button
+          type="button"
+          className="button1 duration-150 hover:bg-[#c3a764]   bg-gray-100 py-2 px-8 rounded-md font-sans "
+        >
+          All
+        </button>
+        <button
+          type="button"
+          className="button1 duration-150 hover:bg-[#c3a764]    bg-gray-100 py-2 px-8 rounded-md font-sans"
+        >
+          WEB
+        </button>
+        <button
+          type="button"
+          className="button1 duration-150 hover:bg-[#c3a764]    bg-gray-100 py-2 px-8 rounded-md font-sans"
+        >
+          GRAPHIC
+        </button>
+      </div>
+      {/* photos */}
+      <div className="photos grid grid-cols-3 gap-4 h-full w-full">
+        <div className=" h-full w-full flex flex-col gap-4"> 
+          <img src="https://www.kaystocks.com/assets/lacasita_lacasita.png" alt="" className="w-full h-auto" />
+         
+          <img src="https://www.kaystocks.com/assets/inthemood_inthemood.png" alt="" className="w-full h-auto" />
+          <img src="https://www.kaystocks.com/assets/dxss_dxss.png" alt="" className="w-full h-auto" />
+          <div className="h-auto w-full bg-red-600"></div>
+        </div>
+        <div className="bg-black h-full w-full"></div>
+        <div className="bg-red-500 h-full w-full"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
